bd99630d4ee444d38a93b5a8c648960611f02c12
Aayush Ghimire <ayushghimire077@gmail.com>
2026-04-19 13:41:19 +0545

    Delete git_push.bat

--- git_push.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-git branch -M main
-git remote add origin https://github.com/aayushghimirey/restaurant-app.git
-git remote set-url origin https://github.com/aayushghimirey/restaurant-app.git
-git add .
-git commit -m "Modernized Finance, Purchase, and Order modules UI/UX; added unified Vendor and Variant searching."
-git push -u origin main
